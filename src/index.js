37691dcc0bafe834a12ecedb5dffe72145d189c6
LucasLucky1 <lucaserickson100@gmail.com>
2023-06-13 20:48:25 -0300

    começando o projeto do zero

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-import { Personagem } from "./modules/personagem.js"
-import { PersonagemView } from "./components/personagem-view.js"
-import { Mago } from "./modules/mago.js"
-import { Arqueiro } from "./modules/arqueiro.js"
-import { ArqueiroMago } from "./modules/arqueiro-mago.js"
-import { Guerreiro } from "./modules/guerreiro.js"
+const personagemPedrinho = {
+    nome: 'Pedrinho',
+    vida: 7,
+    mana: 12,
+    level: 5,
+    tipo: 'Mago',
+}
 
-const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
-const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
-const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
-const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
-const guerreiraJorge = new Guerreiro('Jorge', 8)
+const personagemJose = {
+    nome: 'Jose',
+    vida: 7,
+    mana: 6,
+    level: 3,
+    tipo: 'Arqueiro',
+}
 
-//arqueiroBruno.#level = 15
-
-const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagoChico, guerreiraJorge]
-
-new PersonagemView(personagens).render()
-
-console.log(Personagem.verificarVencedor(arqueiroBruno, magoAntonio))
-
-console.log(arqueiroBruno)
