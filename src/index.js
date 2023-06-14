65d3a2e00e2faffbe9c32c16cfbd48fc1f0d7924
LucasLucky1 <lucaserickson100@gmail.com>
2023-06-13 21:54:50 -0300

    Adicionando Classes, métodos, instâncias e imports iniciais .

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}
+import {Personagem} from "./modules/Personagem.js";
+// const personagempersonagemPedrinho = {
+//     nome: 'personagemPedrinho',
+//     vida: 7,
+//     mana: 12,
+//     level: 5,
+//     tipo: 'Mago',
+// }
 
-const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
+// const personagemJose = {
+//     nome: 'Jose',
+//     vida: 7,
+//     mana: 6,
+//     level: 3,
+//     tipo: 'Arqueiro',
+// }
 
+
+
+const personagemPedrinho = new Personagem();
+personagemPedrinho.nome= 'Pedro';
+personagemPedrinho.vida= 12;
+personagemPedrinho.mana=5;
+personagemPedrinho.level=5;
+personagemPedrinho.tipo= 'Guerreiro';
+
+const personagemJose = new Personagem();
+personagemJose.nome= 'José';
+personagemJose.vida= 7;
+personagemJose.mana=12;
+personagemJose.level=6;
+personagemJose.tipo= 'Mago';
+
+console.log(personagemPedrinho.obterInsignia())
+console.log(personagemJose.obterInsignia())
---
+++ src/modules/Personagem.js
@@ -0,0 +1,20 @@
+
+class Personagem {
+    nome;
+    vida;
+    mana;
+    level;
+    tipo;
+    descricao;
+
+
+    obterInsignia(){
+        if (this.level>5) {
+             return `Implacável ${this.tipo}`
+        }  
+        return `${this.tipo} iniciante`   
+        
+    }
+}
+
+export {Personagem}
